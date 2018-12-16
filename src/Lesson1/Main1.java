594b4f774e9be187b880000576a9c73116879b05
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-12-16 14:39:27 +0400

    класс для теста

--- src/Lesson1/Main1.java
+++ src/Lesson1/Main1.java
@@ -23,14 +23,21 @@ public class Main1 {
             orangeBox.add(new Orange());
         }
 
+        System.out.println("вес коробок:");
+
         System.out.println(appleBox1.getWeight());
         System.out.println(appleBox2.getWeight());
         System.out.println(orangeBox.getWeight());
+
+        System.out.println("сравнение коробок по весу:");
 
         System.out.println(appleBox1.compare(appleBox2));
         System.out.println(appleBox1.compare(orangeBox));
 
         appleBox1.pourTo(appleBox2);
+
+        System.out.println("вес коробки 2 после пересыпания:");
+
         System.out.println(appleBox2.getWeight());
 
 
@@ -40,7 +47,7 @@ public class Main1 {
         String[] strings = {"1", "2", "3", "4", "5"};
         Integer[] integers = {1, 2, 3, 4, 5};
 
-        // меняем местами элементы массива
+        System.out.println("меняем местами элементы массива:");
 
         ArrayModifier.swapElements(strings, 2, 4);
         ArrayModifier.swapElements(integers, 2, 4);
@@ -55,7 +62,7 @@ public class Main1 {
         }
         System.out.println();
 
-        // проверяем метод преобразования массива в коллекцию ArrayList
+        System.out.println("проверяем метод преобразования массива в коллекцию ArrayList:");
 
         ArrayList<String> arrayListSt = ArrayModifier.arrayToArrayList(strings);
         ArrayList<Integer> arrayListInt = ArrayModifier.arrayToArrayList(integers);
