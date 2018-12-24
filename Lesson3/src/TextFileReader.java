17469b8a85c79a8b2d2471b5a27cf30cb8ed12c9
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-12-24 13:56:38 +0400

    Корректно обработана ситуация, когда длина файла делица на страницы без остатка.

--- Lesson3/src/TextFileReader.java
+++ Lesson3/src/TextFileReader.java
@@ -4,11 +4,13 @@ import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class TextFileReader {
+
+    private static final int PAGE_SIZE = 1800;
+
     public static void main(String[] args) {
         System.out.println("Открывается файл textFile.txt");
         try (RandomAccessFile raf = new RandomAccessFile("textFile.txt", "r")) {
-            long numOfPages = raf.length() / 1800 + 1;
-            System.out.println(raf.length());
+            int numOfPages = (int) Math.ceil((double)raf.length() / PAGE_SIZE);
             System.out.println(String.format("В файле %d страниц", numOfPages ));
             Scanner sc = new Scanner(System.in);
             while (true) {
@@ -17,18 +19,18 @@ public class TextFileReader {
                 if (str.startsWith("/q")) {
                     break;
                 }
-                long pageNum = Long.valueOf(str);
-                long offset = (pageNum - 1) * 1800;
+                int pageNum = Integer.valueOf(str);
+                long offset = (pageNum - 1) * PAGE_SIZE;
                 raf.seek(offset);
                 long beginTime = System.currentTimeMillis();
                 if (offset >= raf.length()) {
                     System.out.println(String.format("В файле %d страниц", numOfPages));
                 } else {
                     byte[] pageContent;
-                    if (pageNum == numOfPages) {
+                    if (pageNum == numOfPages && raf.length() % PAGE_SIZE > 0) {
                         pageContent = new byte[(int) (raf.length() - offset)];
                     } else {
-                        pageContent = new byte[1800];
+                        pageContent = new byte[PAGE_SIZE];
                     }
                     raf.readFully(pageContent);
                     System.out.println(new String(pageContent));
