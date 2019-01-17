3aae33e0a051526ee26f7e54e41863f8e4a9fc6d
Vladim1r37 <vl4d1m1r37@gmail.com>
2019-01-17 20:57:02 +0400

    реализация методов записи и чтения данных из таблицы Students

--- Lesson6/src/main/java/dz/repo/StudentsDAOImpl.java
+++ Lesson6/src/main/java/dz/repo/StudentsDAOImpl.java
@@ -1,4 +1,46 @@
 package dz.repo;
 
-public class StudentsDAOImpl {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class StudentsDAOImpl implements StudentsDAO {
+
+    private Connection connection;
+
+    public StudentsDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public int addRecord(String name, int score) {
+        try {
+            return connection.createStatement().executeUpdate(String.format("INSERT INTO students (name, score) " +
+                    "VALUES ('%s', %d)", name, score));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public int updateScore(String name, int score) {
+        try {
+            return connection.createStatement().executeUpdate(String.format("UPDATE students " +
+                    "SET score = %d WHERE name = '%s'", score, name));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public int readScore(String name) {
+        try {
+            return connection.createStatement().executeQuery(String.format("SELECT score " +
+                    "FROM students WHERE name = '%s'", name)).getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
 }
