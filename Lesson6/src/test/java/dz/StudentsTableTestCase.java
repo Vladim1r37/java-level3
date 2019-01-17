55a2368cebc22c3c305271926463b9a3b49c49d6
Vladim1r37 <vl4d1m1r37@gmail.com>
2019-01-17 20:58:08 +0400

    тест записи и чтения из таблицы Students

--- Lesson6/src/test/java/dz/StudentsTableTestCase.java
+++ Lesson6/src/test/java/dz/StudentsTableTestCase.java
@@ -1,4 +1,64 @@
 package dz;
 
+import dz.repo.StudentsDAO;
+import dz.repo.StudentsDAOImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StudentsTableTestCase {
+
+    private StudentsDAO dao;
+    private SQLiteService service;
+
+    @Before
+    public void prepare() {
+        this.service = new SQLiteService("sample.db");
+        this.dao = new StudentsDAOImpl(service.getConnection());
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            service.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAddRecord1() {
+        Assert.assertEquals(1, dao.addRecord("Ivanov", 5));
+    }
+
+    @Test
+    public void testAddRecord2() {
+        Assert.assertEquals(1, dao.addRecord("Petrov", 3));
+    }
+
+    @Test
+    public void testUpdateScore1() {
+        Assert.assertEquals(1, dao.updateScore("Petrov", 4));
+    }
+
+    @Test
+    public void testUpdateScore2() {
+        Assert.assertEquals(0, dao.updateScore("Sidorov", 4));
+    }
+
+    @Test
+    public void testUpdateScore3() {
+        Assert.assertEquals(1, dao.updateScore("Petrov", 4));
+    }
+
+    @Test
+    public void testReadScore1() {
+        Assert.assertEquals(3, dao.readScore("Petrov"));
+    }
+
+    @Test
+    public void testReadScore2() {
+        Assert.assertEquals(0, dao.readScore("Sidorov"));
+    }
 }
