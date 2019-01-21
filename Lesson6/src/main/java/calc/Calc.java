0367446de129df9b3563c557abc8ab5b7a01c306
Vladim1r37 <vl4d1m1r37@gmail.com>
2019-01-21 17:20:48 +0400

    классы из примера

--- Lesson6/src/main/java/calc/Calc.java
+++ Lesson6/src/main/java/calc/Calc.java
@@ -1,4 +1,23 @@
 package calc;
 
-public class Calc {
+public class Calc implements ICalc {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int div(int a, int b) {
+        return a / b;
+    }
 }
--- Lesson6/src/main/java/calc/ICalc.java
+++ Lesson6/src/main/java/calc/ICalc.java
@@ -1,4 +1,13 @@
 package calc;
 
 public interface ICalc {
+
+    int add(int a, int b);
+
+    int sub(int a, int b);
+
+    int mul(int a, int b);
+
+    int div(int a, int b);
+
 }
