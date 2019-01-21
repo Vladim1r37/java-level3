7f58ec32f3a3ee60f27fb7c2b9011050f7648800
Vladim1r37 <vl4d1m1r37@gmail.com>
2019-01-21 17:21:31 +0400

    классы с примерами разных вариантов логирования

--- Lesson6/src/main/java/log/Log4j_Example.java
+++ Lesson6/src/main/java/log/Log4j_Example.java
@@ -1,4 +1,17 @@
-package log;
-
-public class Log4j_Example {
-}
+//package log;
+//
+//
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//public class Log4j_Example {
+//
+//    public static final Logger log = LogManager.getLogger(Log4j_Example.class);
+//
+//    public static void main(String[] args) {
+//
+//        log.debug("Debug message logged!");
+//        log.info("Info message logged! My name is {}", "MyName");
+//        log.error("Error message logged!", new NullPointerException("Null Error"));
+//    }
+//}
